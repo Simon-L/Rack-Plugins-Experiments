3ea719ba76aa47e8c4a4aeefcc3a441d9d0b20f5
Simon-L <lumis@xulepth.fr>
2022-06-04 22:58:39 +0200

    POC: v0.1 Overkill but only solution I found for headless dynamic cables Try running headless or not to see the difference :)

--- src/HeadlessPatcher.cpp
+++ src/HeadlessPatcher.cpp
@@ -1,7 +1,15 @@
 #include "plugin.hpp"
+#include <thread>
 
 // #define PROCESS
-#define STEP
+// #define STEP
+#define THREAD
+#define DELAY 3.0
+
+struct ThreadMsg
+{
+	uint8_t type;
+};
 
 struct HeadlessPatcher : Module {
 	enum ParamId {
@@ -32,6 +40,17 @@ struct HeadlessPatcher : Module {
 		LIGHTS_LEN
 	};
 
+	void onSave(const SaveEvent& e) override {
+		DEBUG("onSave");
+		if (settings::headless) {
+			ThreadMsg msg;
+			msg.type = 3;
+			DEBUG("Sending remove cable action! %d empty? %d", msg.type, ch.empty());
+			ch.push(msg);
+			while (!ch.empty()) DEBUG("Waiting...");
+		}
+	}
+
 	HeadlessPatcher() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(K1_PARAM, 0.f, 1.f, 0.f, "K1_PARAM");
@@ -48,55 +67,147 @@ struct HeadlessPatcher : Module {
 		// configOutput(TRI_OUTPUT, "");
 		// configOutput(SAW_OUTPUT, "");
 		// configOutput(SQR_OUTPUT, "");
+		ch.clear();
 	}
 
+	size_t uiThreadId;
+	dsp::RingBuffer<ThreadMsg, 64> ch;
 	dsp::Timer mytimer;
 	dsp::BooleanTrigger mytoggle;
 	void process(const ProcessArgs& args) override {
 		mytimer.process(args.sampleTime);
 #ifdef PROCESS
-		if (mytoggle.process((mytimer.getTime() > 0.4))) {
+		if (mytoggle.process((mytimer.getTime() > DELAY))) {
 			DEBUG("process()");
-			goCableGo();
+			addMyCable(APP);
+		}
+#endif
+#ifdef THREAD
+		if (mytoggle.process((mytimer.getTime() > DELAY))) {
+			ThreadMsg msg;
+			msg.type = 1;
+			DEBUG("Sending! %d empty? %d", msg.type, ch.empty());
+			ch.push(msg);
 		}
 #endif
 	}
 
-	void goCableGo() {
+	void addMyCable(rack::Context* ctx) {
 		DEBUG("Cable!");
 
 		Cable* cable = new Cable;
-		cable->outputModule = APP->engine->getModule(0x184bbb8c0bfc92);
+		cable->outputModule = ctx->engine->getModule(0x184bbb8c0bfc92);
 		cable->outputId = 1;
-		cable->inputModule = APP->engine->getModule(0x5a28fcc5f65ce);
+		cable->inputModule = ctx->engine->getModule(0x5a28fcc5f65ce);
 		cable->inputId = 3;
+		cable->id = 0xdeadbeef1234;
 
-		APP->engine->addCable(cable);
-		rack::app::CableWidget* cw = new rack::app::CableWidget;
-		cw->setCable(cable);
-		cw->color = NVGcolor{0.76f, 0.11f, 0.22f, 1.00f};
-		cw->updateCable();
-		if (cw->isComplete()) {
-			DEBUG("Cable complete!");
-	    	APP->scene->rack->addCable(cw);
+		ctx->engine->addCable(cable);
+
+		if (std::hash<std::thread::id>{}(std::this_thread::get_id()) == uiThreadId) {
+			DEBUG("On the right thread :) !");
+			rack::app::CableWidget* cw = new rack::app::CableWidget;
+			cw->color = NVGcolor{0.76f, 0.11f, 0.22f, 1.00f};
+			cw->setCable(cable);	
+			cw->updateCable();
+			if (cw->isComplete()) {
+				DEBUG("Cable complete!");
+				ctx->scene->rack->addCable(cw);
+			}
+		} else {
+			DEBUG("Not the right thread sorry :/");
 		}
 	}
 };
 
+#ifdef THREAD
+void threadfunc(HeadlessPatcher* module, rack::Context* ctx) {
+	while (true) {
+		if (!module->ch.empty()) {
+			ThreadMsg msg = module->ch.shift();
+			DEBUG("Receiving msg: %d", msg.type);
+			switch (msg.type) {
+				case 1:
+					module->addMyCable(ctx);
+					break;
+				case 2:
+					DEBUG("Stopping bg thread!");
+					return;
+					break;
+				case 3:
+					DEBUG("Removing cable from thread because headless!");
+					Cable* cab = ctx->engine->getCable(0xdeadbeef1234);
+					ctx->engine->removeCable(cab);
+					break;
+			}
+			module->ch.clear();
+		}
+	}
+}
+#endif
 
 struct HeadlessPatcherWidget : ModuleWidget {
 
-#ifdef STEP
-	void step() override {
+	void onRemove (const RemoveEvent &e) override {
+		DEBUG("ModuleWidget::onRemove()");
+#ifdef THREAD
 		if (module) {
 			HeadlessPatcher* _module = dynamic_cast<HeadlessPatcher*>(module);
-			if (_module->mytoggle.process((_module->mytimer.getTime() > 0.4))) {
-				DEBUG("step()");
-				_module->goCableGo();
+			if (_module) {
+				ThreadMsg msg;
+				msg.type = 2;
+				DEBUG("Sending! %d empty? %d", msg.type, _module->ch.empty());
+				_module->ch.push(msg);	
+				DEBUG("Joining...?");
+				mythread.join();
+				DEBUG("Joined!");
+			}
+		}
+#endif
+		ModuleWidget::onRemove(e);
+	}
+	void onContextDestroy (const ContextDestroyEvent &e) override {
+		DEBUG("ModuleWidget::onContextDestroy()");
+#ifndef STEP
+		if (!settings::headless) {
+			Cable* cab = APP->engine->getCable(0xdeadbeef1234);
+			APP->engine->removeCable(cab);
+			DEBUG("Cable deleted in onContextDestroy because *not* headless and not STEP");
+		}
+#endif
+		ModuleWidget::onContextDestroy(e);
+	}
+
+#ifdef THREAD
+	std::thread mythread;
+#endif
+
+	void onAdd (const AddEvent &e) override {
+		DEBUG("onAdd()");
+		if (module) {
+			HeadlessPatcher* _module = dynamic_cast<HeadlessPatcher*>(module);
+			if (_module) {
+				_module->uiThreadId = std::hash<std::thread::id>{}(std::this_thread::get_id());
+#ifdef THREAD
+				mythread = std::thread{threadfunc, _module, APP};
+#endif
 			}
 		}
 	}
+
+	void step() override {
+		if (module) {
+			HeadlessPatcher* _module = dynamic_cast<HeadlessPatcher*>(module);
+			if (_module) {
+#ifdef STEP
+				if (_module->mytoggle.process((_module->mytimer.getTime() > DELAY))) {
+					DEBUG("step()");
+					_module->addMyCable(APP);
+				}
 #endif
+			}
+		}
+	}
 
 	HeadlessPatcherWidget(HeadlessPatcher* module) {
 		setModule(module);
